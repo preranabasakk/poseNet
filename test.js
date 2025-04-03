b216d2df6326b5ec142b105f99de00ec814aa669
preranabasakk <basakprerna7@gmail.com>
2025-04-03 10:24:07 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,12 @@
+// let capture;
+
+// function setup() {
+//     createCanvas(800, 500);
+//     capture = createCapture(VIDEO);
+//     capture.size(800, 500); // set the capture size
+//     capture.hide();
+// }
+
+// function draw() {
+//     image(capture, 0, 0);
+// }
